a3c89fc20e566aebaab2c9607a8f9a22584777d1
Jake Furlong <jake.w.furlong@gmail.com>
2025-05-17 14:26:35 -0700

    added all resoruces for custom vpc

---
+++ modules/vpc/main.tf
@@ -0,0 +1,75 @@
+resource "aws_vpc" "custom_vpc" {
+  cidr_block       = var.aws_vpc_cidr_block
+
+  tags = {
+    Name = "vpc-${var.aws_network_name}"
+  }
+}
+
+resource "aws_internet_gateway" "custom_igw" {
+  vpc_id = aws_vpc.custom_vpc.id
+
+  tags = {
+    Name = "igw-${var.aws_network_name}"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.custom_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.custom_igw.id
+  }
+
+  tags = {
+    Name = "rt-public-${var.aws_network_name}"
+  }
+}
+
+resource "aws_subnet" "public" {
+  for_each = toset(var.azs)
+
+  vpc_id                  = aws_vpc.custom_vpc.id
+  cidr_block              = cidrsubnet(var.aws_vpc_cidr_block, 8, index(var.azs, each.key))
+  availability_zone       = each.key
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "subnet-public-${each.key}"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  for_each = aws_subnet.public
+
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_security_group" "default_sg" {
+  name        = "default-sg"
+  description = "Default SG allowing all traffic within the VPC"
+  vpc_id      = aws_vpc.custom_vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [var.aws_vpc_cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "sg-${var.aws_network_name}"
+  }
+}
+
+
+
---
+++ modules/vpc/outputs.tf
@@ -0,0 +1,23 @@
+output "vpc_id" {
+  value = aws_vpc.custom_vpc.id
+}
+
+output "internet_gateway_id" {
+  value = aws_internet_gateway.custom_igw.id
+}
+
+output "route_table_id" {
+  value = aws_route_table.public_rt.id
+}
+
+output "public_subnet_ids" {
+  value = [for s in aws_subnet.public : s.id]
+}
+
+output "route_table_association_ids" {
+  value = [for rta in aws_route_table_association.public : rta.id]
+}
+
+output "security_group_id" {
+  value = aws_security_group.default_sg.id
+}
