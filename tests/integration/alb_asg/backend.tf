8ca5f72fcc3a860f1a502aabb7a57f6859adbad8
Jake Furlong <jake.w.furlong@gmail.com>
2025-05-19 14:41:30 -0700

    removed integration

--- tests/integration/alb_asg/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "nimbusdevops"
-    key            = "terraform-state/tests/integration/alb_asg/terraform.tfstate"
-    region         = "us-west-1"
-    encrypt        = true
-    use_lockfile   = true
-  }
-}
