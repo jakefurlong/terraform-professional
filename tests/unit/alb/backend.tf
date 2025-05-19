dd6af4c9ef6de0a0ccd725b5dd8e42b39efc1ea3
Jake Furlong <jake.w.furlong@gmail.com>
2025-05-18 20:33:36 -0700

    terraform ftm -recursive

--- tests/unit/alb/backend.tf
+++ tests/unit/alb/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "nimbusdevops"
-    key            = "terraform-state/tests/unit/alb/terraform.tfstate"
-    region         = "us-west-1"
-    encrypt        = true
-    use_lockfile   = true
+    bucket       = "nimbusdevops"
+    key          = "terraform-state/tests/unit/alb/terraform.tfstate"
+    region       = "us-west-1"
+    encrypt      = true
+    use_lockfile = true
   }
 }
