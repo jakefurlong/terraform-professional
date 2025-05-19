45e0aecac118e974de970996866f62fcff1d6c5e
Jake Furlong <jake.w.furlong@gmail.com>
2025-05-18 20:24:46 -0700

    added remote state backend for all tests

---
+++ tests/unit/alb/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "your-terraform-state-bucket"
+    key            = "terraform-state/tests/unit/alb/terraform.tfstate"
+    region         = "us-west-1"
+    encrypt        = true
+    use_lockfile   = true
+  }
+}
