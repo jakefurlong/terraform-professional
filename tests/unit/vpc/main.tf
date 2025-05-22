fb4b5a29e00e9e4c270a4667dda96b41783cffc6
Jake Furlong <jake.w.furlong@gmail.com>
2025-05-21 17:27:32 -0700

    updated main to point to github source

--- tests/unit/vpc/main.tf
+++ tests/unit/vpc/main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source = "../../../modules/vpc"
+  source = "git::https://github.com/jakefurlong/terraform-modules.git//vpc?ref=v1.0.1"
 
   aws_vpc_cidr_block = "10.0.0.0/16"
   aws_network_name   = "nimbusdevops-test"
